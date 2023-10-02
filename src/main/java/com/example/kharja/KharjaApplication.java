2c9d217df40f56993071897c541965b9c9ffee3a
bairemkh <bairemkh@gmail.com>
2023-10-02 18:23:42 +0100

    images fid

--- src/main/java/com/example/kharja/KharjaApplication.java
+++ src/main/java/com/example/kharja/KharjaApplication.java
@@ -1,14 +1,22 @@
 package com.example.kharja;
 
 
+import com.example.kharja.Services.FilesStorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class KharjaApplication {
+import javax.annotation.Resource;
 
+@SpringBootApplication
+public class KharjaApplication implements CommandLineRunner {
+    @Resource
+    FilesStorageService storageService;
     public static void main(String[] args) {
         SpringApplication.run(KharjaApplication.class, args);
     }
-
+   @Override
+    public void run(String... args) throws Exception {
+        storageService.init();
+    }
 }
---
+++ src/test/java/com/example/kharja/Controllers/NightClubTest.java
@@ -0,0 +1,47 @@
+package com.example.kharja.Controllers;
+
+
+import com.example.kharja.Entity.NightClub;
+import com.example.kharja.Services.NightClubService;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+
+@SpringBootTest
+public class NightClubTest {
+
+    @InjectMocks
+    private NightClubController nightClubController;
+
+    @Mock
+    private NightClubService nightClubService;
+
+    @Before("")
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testAddNightClub() {
+        NightClub nightClub = new NightClub();
+        NightClub nightClub1 = new NightClub();
+        nightClub.setName("Test Night Club 1");
+        nightClub1.setName("Test Night Club 2");
+        when(nightClubService.add(any(NightClub.class))).thenReturn(nightClub);
+        ResponseEntity<Object> responseEntity = nightClubController.addNightClub(nightClub);
+        verify(nightClubService, times(1)).add(nightClub);
+        assertNotNull(responseEntity);
+        assertEquals(ResponseEntity.accepted().body(nightClub), responseEntity);
+    }
+}
